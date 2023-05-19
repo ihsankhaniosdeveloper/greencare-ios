404810fc8306f5f1730d894c80d52ab6f4cd42a6
Rashid Khan <rashidkhan@elgrocer.com>
2023-05-19 21:10:13 +0500

    address table view cell added, delegate and data source basic setup.

--- Gardner/Presentation/Listing/AddressListingViewController.swift
+++ Gardner/Presentation/Listing/AddressListingViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class AddressListingViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var addressList: [Address] = []
+    
     private var presenter: AddressListingPresenterType!
     
     static func make(presenter: AddressListingPresenterType) -> AddressListingViewController {
@@ -23,6 +27,11 @@ class AddressListingViewController: UIViewController {
 
         self.title = "My Addresses"
         self.navigationController?.navigationBar.isHidden = false
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.tableView.register(UINib(nibName: "AddressTableViewCell", bundle: .main), forCellReuseIdentifier: "AddressTableViewCell")
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -31,4 +40,18 @@ class AddressListingViewController: UIViewController {
         self.navigationController?.navigationBar.isHidden = true
     }
 
+}
+
+extension AddressListingViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.addressList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AddressTableViewCell", for: indexPath) as! AddressTableViewCell
+        
+        cell.configure(address: self.addressList[indexPath.row])
+        
+        return cell
+    }
 }
---
+++ Gardner/Presentation/Listing/Cell/AddressTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  AddressTableViewCell.swift
+//  Gardner
+//
+//  Created by Rashid Khan on 19/05/2023.
+//
+
+import UIKit
+
+class AddressTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.selectionStyle = .none
+    }
+    
+    func configure(address: Address) {
+        
+    }
+    
+}
