22c3c7ba8409f648a24c542f9dd1c7ee6ba1843f
Rashid Khan <rashidkhan@elgrocer.com>
2023-06-23 00:55:39 +0500

    notification listing controlleer added.

---
+++ Gardner/Presentation/Notifications/NotificationListingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NotificationListingViewController.swift
+//  Gardner
+//
+//  Created by Rashid Khan on 23/06/2023.
+//
+
+import UIKit
+
+class NotificationListingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
